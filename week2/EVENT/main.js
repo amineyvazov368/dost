ae9e5215502c94768656fbe5f27569531bcc1836
amin12345 <tu81xz8hx@code.edu.az>
2024-04-09 13:13:16 +0400

    event deyisikik var

--- week2/EVENT/main.js
+++ week2/EVENT/main.js
@@ -5,33 +5,50 @@ let companyId=document.querySelector(".company-id")
 let formInputs=document.querySelectorAll(".form-input")
 let add=document.querySelector(".add")
 let myTable=document.querySelector(".mytable")
+let clear=document.querySelectorAll(".btn-clear")
+
+
+
+class newProduct{
+    static companyId=3
+    isCompleted;
+    constructor(companyName,contactName,contactTitle){
+        this.companyId=newProduct.companyId++
+        this.companyName=companyName,
+        this.contactName=contactName,
+        this.contactTitle=contactTitle
+    }
+}
 add.addEventListener("click",function(e){
     e.preventDefault()
     let newproduct
     formInputs.forEach(formInput=>{
         if(formInput.value==""){
-            formInput.nextElementSibling.nextElementSibling.classList.add("active")
+            formInput.nextElementSibling.classList.add("active")
         }else{
-            formInput.nextElementSibling.nextElementSibling.classList.remove("active")
-
-             newproduct={
-                companyName:companyName.value,
-                contactName:companyName.value,
-                contactTitle:contactTitle.value,
-                companyId:companyId.value
-            }
+            formInput.nextElementSibling.classList.remove("active")
+            newproduct=new newProduct(companyName.value,contactName.value,contactTitle.value)
         }
     })
-    showProduct(newproduct)
+    showProduct(newproduct);
 })
- function showProduct(product){
-        myTable.innerHTML+=`
-        <tr>
-        <td>${product.companyName} </td>
-        <td>${product.contactName} </td>
-        <td>${product.contactTitle} </td>
-        <td>${product.companyId}</td>
-    </tr> 
-        `
-        
- }
+function showProduct(product){
+
+    myTable.innerHTML+=`
+    <tr>
+    <td> ${product.companyId}</td>
+    <td> ${product.contactName}</td>
+    <td> ${product.contactTitle}</td>
+    <td>${product.companyName}</td>
+    <td><button>delete</button></td>
+</tr>   `
+
+companyName.value="",
+contactName.value="",
+contactTitle.value="",
+clear.forEach(clear=>{
+    clear.addEventListener("click",function(){
+        clear.closest(".mytable").remove()
+    })
+})
+}
