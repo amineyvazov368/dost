64f1a2d537b5d9ba897d08fde29b70e3caeea20a
amin12345 <tu81xz8hx@code.edu.az>
2024-04-24 14:02:22 +0400

    taski add etdim

---
+++ reactTask1/src/conpanent/Shows.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Shows.css'
+
+ const Shows = () => {
+  return (
+    <div className='title'>
+       <h1>Shop in style</h1> <br/>
+       <p>Lorem ipsum dolor sit amet consectetur.</p>
+    </div>
+  )
+}
+
+export default Shows
